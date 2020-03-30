87883736dd6d5508b04470a510c711a2634646dc
Nan Luo <nan.luo@shopee.com>
2020-03-30 19:32:35 +0800

    First Commit 2

---
+++ README.md
@@ -0,0 +1,19 @@
+# ViroSample
+
+## How to run?
+Official Version:
+npm start
+then open Viro Media App on your device, click `Enter Testbed` menu, input http://{YOUR_MACHINE_IP}:/8081
+
+RN Version:
+Run `./setup-ide.sh --android` in root path, then run `npx react-native start ` in one terminal,  run `npx react-native run-android`.
+The rn start command would start a server listening on port 8081.
+
+
+## RN prepare
+upgrade Node to .12+
+use `npx react-native` command not `react-native-cli`. (you should uninstall react-native-cli command first)
+
+## ref
+https://blog.jscrambler.com/getting-started-with-vr-in-react-native-with-viroreact/
+https://marmelab.com/blog/2019/04/25/react-native-augmented-reality.html
---
+++ js/HelloWorldSceneAR.js
@@ -0,0 +1,127 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {StyleSheet} from 'react-native';
+
+import {
+	ViroARScene,
+	ViroText,
+	ViroBox,
+	ViroConstants,
+	ViroMaterials,
+	Viro3DObject,
+	ViroAmbientLight,
+	ViroSpotLight,
+	ViroARPlaneSelector,
+	ViroNode,
+	ViroAnimations,
+} from "react-viro";
+
+export default class HelloWorldSceneAR extends Component {
+
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text : "Initializing AR..."
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  render() {
+    // 猜测使用右手坐标系  因为人面对的是 -z 方向,  右手边是 x 的正方向  左手边 x 福相
+    return (
+			<ViroARScene onTrackingUpdated={this._onInitialized}>
+				<ViroNode
+					position={[0, -1, 0]}
+					dragType="FixedToWorld"
+					onDrag={() => {}}
+				>
+					<Viro3DObject
+						source={require("./res/emoji_smile/emoji_smile.vrx")}
+						resources={[
+							require("./res/emoji_smile/emoji_smile_diffuse.png"),
+							require("./res/emoji_smile/emoji_smile_normal.png"),
+							require("./res/emoji_smile/emoji_smile_specular.png")
+						]}
+						position={[0, 0.5, 0]}
+						scale={[0.2, 0.2, 0.2]}
+						type="VRX"
+					/>
+				</ViroNode>
+
+				<ViroText
+					text={this.state.text}
+					scale={[0.5, 0.5, 0.5]}
+					position={[0, 0, -1]}
+					style={styles.helloWorldTextStyle}
+				/>
+				<ViroBox
+					position={[0, -0.5, -1]}
+					scale={[0.3, 0.3, 0.1]}
+					materials={["grid"]}
+					animation={{ name: "rotate", run: true, loop: true }}
+				/>
+
+				<ViroAmbientLight color={"#aaaaaa"} />
+				<ViroSpotLight
+					innerAngle={5}
+					outerAngle={90}
+					direction={[0, -1, -0.2]}
+					position={[0, 3, 1]}
+					color="#ffffff"
+					castsShadow={true}
+				/>
+				<Viro3DObject
+					source={require("./res/emoji_smile/emoji_smile.vrx")}
+					resources={[
+						require("./res/emoji_smile/emoji_smile_diffuse.png"),
+						require("./res/emoji_smile/emoji_smile_normal.png"),
+						require("./res/emoji_smile/emoji_smile_specular.png")
+					]}
+					position={[-0.5, 0.5, -1]}
+					scale={[0.2, 0.2, 0.2]}
+					type="VRX"
+				/>
+			</ViroARScene>
+		);
+  }
+
+  _onInitialized(state, reason) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        text : "Hello World!"
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+}
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
+ViroMaterials.createMaterials({
+	grid: {
+		diffuseTexture: require("./res/grid_bg.jpg")
+	}
+});
+ViroAnimations.registerAnimations({
+	rotate: {
+		properties: {
+			rotateY: "+=90"
+		},
+		duration: 250 //.25 seconds
+	}
+});
+module.exports = HelloWorldSceneAR;
