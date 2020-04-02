44c6f696e6ae8605dfd9da6c46244228e1955847
Nan Luo <nan.luo@shopee.com>
2020-04-02 17:21:18 +0800

    add rn send msg to webview

--- js/ARWebview/ARWebview.js
+++ js/ARWebview/ARWebview.js
@@ -31,6 +31,7 @@ import { Dimensions } from "react-native";
 var stageWidth = Dimensions.get('window').width; //full width
 var stageHeight = Dimensions.get('window').height; //full height
 var absViewHeight = 650
+var webviewURL = "http://10.12.167.120:5001/";
 export default class ARWebview extends Component {
 	constructor() {
 		super();
@@ -42,33 +43,63 @@ export default class ARWebview extends Component {
 
 		// bind 'this' to functions
 		console.log("this rootview..", this.refs["rootView"])
+
+
+		setTimeout(() => {
+			this.postToWebview();
+		}, 1000);
 	}
 
 
+	postToWebview(){
+		var runJS = `
+			window.GLOBAL_VAR.aa = 11;
+			true;
+		`;
+		this.webref.injectJavaScript(runJS);
+	}
+
+	onMessage(event) {
+		console.log("RCT  recevice event", event.nativeEvent.data);
+	}
 
 	render() {
+
+		// 这里还是属于 页面向 RN 发送数据  只不过页面的数据是 RN 通过inject 方式引入
+		const INJECTED_POST_JS_FIRST = `(function() {
+				setTimeout(function(){
+					window.ReactNativeWebView.postMessage(JSON.stringify( { lo: "lololo" } ));
+				}, 1000)
+				true;
+		})();`;
+
+		const RUNFIRST = `
+      document.body.style.backgroundColor = 'red';
+      setTimeout(function() { window.alert('hi') }, 2000);
+      true; // note: this is required, or you'll sometimes get silent failures
+    `;
+
 		return (
 			<View ref="rootView" style={styles.outer}>
 				<View style={styles.bgView}></View>
 
 				<View style={styles.absView}>
 					<WebView
+						ref={r => (this.webref = r)}
 						style={styles.webview}
+						injectedJavaScript={INJECTED_POST_JS_FIRST}
+						onMessage={this.onMessage}
 						source={{
-							uri: "https://google.com/"
+							uri: webviewURL
 						}}
 					/>
-
 				</View>
 				{/* <ViroARSceneNavigator
 					style={styles.bgView}
 					initialScene={{ scene: InitialARScene }}
 				></ViroARSceneNavigator> */}
 
-
 				{/* <WebView style={styles.webview} source={{ uri: "https://reactnative.dev/" }} /> */}
-				{/*
-				 */}
 			</View>
 		);
 	}
---
+++ webpage/index.js
@@ -0,0 +1,16 @@
+window.addEventListener("message", receiveMessage, false);
+var infoDiv = document.querySelector("#info")
+function receiveMessage(event) {
+	// For Chrome, the origin property is in the event.originalEvent
+	// object.
+	// 这里不准确，chrome没有这个属性
+	// var origin = event.origin || event.originalEvent.origin;
+	var origin = event.origin;
+	console.log("event::::", event.data)
+
+
+  if (window.ReactNativeWebView) {
+		window.ReactNativeWebView.postMessage("Hello! From JS");
+	}
+	// ...
+}
